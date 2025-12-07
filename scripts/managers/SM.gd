dae27276dfa81f3f22de06ba1d0f721b17c62f63
Minejerik <jaanerikf@gmail.com>
2025-12-07 11:57:43 -0500

    Working on settings + new level

---
+++ scripts/managers/SM.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var SETTINGS = {
+	"volume": 50
+}
--- scripts/ui/settings.gd
+++ scripts/ui/settings.gd
@@ -1,4 +1,10 @@
 extends Window
 
+func _ready() -> void:
+	$ColorRect/MarginContainer/VBoxContainer/HBoxContainer/VolumeSlider.value = SM.SETTINGS["volume"]
+
 func _on_close_requested() -> void:
 	hide()
+
+func _on_settings_save_pressed() -> void:
+	SM.SETTINGS["volume"] = $ColorRect/MarginContainer/VBoxContainer/HBoxContainer/VolumeSlider.value
